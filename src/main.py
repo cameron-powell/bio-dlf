3eeb6633943bcb54c8be6220b76e0a1a402ac116
Cameron <cameronspowell@gmail.com>
2017-02-09 21:20:27 -0500

    Created a feedback page
    Updated base.html to show a flash message (success) if they submitted feedback.  Updated main to route everything for the feedback page.  Updated requirements.txt to show the libraries we're using up to this point.

--- src/main.py
+++ src/main.py
@@ -1,14 +1,40 @@
+from flask import flash
 from flask import Flask
+from flask import redirect
 from flask import render_template
+from flask import url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+from wtforms import SubmitField
+from wtforms import TextAreaField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'temp key' #TODO: This is only for debugging.  Change to environment variable for production use.
 bootstrap = Bootstrap(app)
+
+
+class FeedbackForm(FlaskForm):
+    section = SelectField('Lab Section',
+                          choices=[('P01', 'P01 Monday 1:05pm-3:55pm'), ('Q05', 'Q05 Wednesday 1:05pm-3:55pm')])
+    feedback = TextAreaField('Feedback', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
 def index():
     return render_template('base.html')
+
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    form = FeedbackForm()
+    if form.validate_on_submit():
+        flash('Thank you for your feedback!')
+        return redirect(url_for('index'))
+    return render_template('feedback.html', form=form)
 
 
 @app.errorhandler(404)
