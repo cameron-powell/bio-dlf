8fe438cb22f7269d5af22de1733c47b084d979a2
Cameron <cameronspowell@gmail.com>
2017-02-10 23:24:03 -0500

    Added functionality to send email once feedback is submitted
    Created the templates/mail directory for mail templates.  Created the new_feedback.html and .txt files which will be used when sending email.  Modified main.py to use flask mail.  Configured the email settings using environment variables.  Generated a secret key and moved that to environment variables.  Created the send_email function to simplify sending mail.  Modified the feedback view to grab the data from both the section and feedback areas of the form and send an email containing the data to the configured address.  Updated the requirements.txt since new libraries are being used.

--- src/main.py
+++ src/main.py
@@ -4,7 +4,10 @@ from flask import redirect
 from flask import render_template
 from flask import url_for
 from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_mail import Message
 from flask_wtf import FlaskForm
+import os
 from wtforms import SelectField
 from wtforms import SubmitField
 from wtforms import TextAreaField
@@ -12,8 +15,31 @@ from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'temp key' #TODO: This is only for debugging.  Change to environment variable for production use.
+# Secret key setup
+app.config['SECRET_KEY'] = os.environ.get('BIO_DLF_SECRET_KEY')
+# Email setup
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('BIO_DLF_MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('BIO_DLF_MAIL_PASSWORD')
+app.config['MAIL_SUBJECT_PREFIX'] = 'Someone has submitted feedback!'
+app.config['MAIL_SENDER'] = os.environ.get('BIO_DLF_MAIL_USERNAME')
+app.config['MAIL_RECEIVER'] = os.environ.get('BIO_DLF_MAIL_RECEIVER')
+mail = Mail(app)
+# Bootstrap setup
 bootstrap = Bootstrap(app)
+
+
+def send_email(to, subject, template, **kwargs):
+    """Call this function when wanting to send an email.  The subject passed in will be prefixed with the value of
+    app.config['MAIL_SUBJECT_PREFIX'], so if that needs to be changed change it where it's set above.
+    Emails will always be sent from app.config['MAIL_SENDER'].  The recipient needs to be passed in in the to variable.
+    The template passed in needs to exist within the templates/mail directory and have both a .html and .txt version."""
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX']+subject, sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 class FeedbackForm(FlaskForm):
@@ -32,7 +58,14 @@ def index():
 def feedback():
     form = FeedbackForm()
     if form.validate_on_submit():
+        # Get the data from the form
+        section_data = form.section.data
+        feedback_data = form.feedback.data
+        # Send an email containing the feedback
+        send_email(app.config['MAIL_RECEIVER'], ' '+section_data, 'mail/new_feedback', feedback_data=feedback_data)
+        # Tell the user that their feedback was submitted
         flash('Thank you for your feedback!')
+        # Go back to the home page
         return redirect(url_for('index'))
     return render_template('feedback.html', form=form)
 
