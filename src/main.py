303bdf486f658dd1a1aebe9954eb510e2bc78024
Cameron <cameronspowell@gmail.com>
2017-02-22 22:19:07 -0500

    Created a 500 error page template. Resolves #4
    Edited the main.py file's internal server error view function to render the 500.html template.  Also created the 500.html template.  Simply displays that there was an internal server error.

--- src/main.py
+++ src/main.py
@@ -84,7 +84,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def internal_server_error(e):
-    return '<h1>500</h1>', 500
+    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
