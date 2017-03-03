700a34ce80ba64900ba1ad8d022abb0f829bb9ef
Cameron <cameronspowell@gmail.com>
2017-03-03 00:30:23 -0500

    Updated configuration settings

--- config.py
+++ config.py
@@ -5,8 +5,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('BIO_DLF_SECRET_KEY')
+    MAIL_PASSWORD = os.environ.get('BIO_DLF_MAIL_PASSWORD')
+    MAIL_PORT = 587
     MAIL_RECEIVER = os.environ.get('BIO_DLF_MAIL_RECEIVER')
+    MAIL_SENDER = os.environ.get('BIO_DLF_MAIL_USERNAME')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('BIO_DLF_MAIL_USERNAME')
+    SECRET_KEY = os.environ.get('BIO_DLF_SECRET_KEY')
 
     @staticmethod
     def init_app(app):
@@ -15,11 +21,6 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('BIO_DLF_MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('BIO_DLF_MAIL_PASSWORD')
 
 
 class ProductionConfig(Config):
