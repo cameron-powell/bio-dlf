0c5eba500244857548069f9b6572bcba99e38972
Cameron <cameronspowell@gmail.com>
2017-03-02 23:19:31 -0500

    Moved forms to the forms section

---
+++ app/main/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import SelectField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+
+
+class IndexForm(FlaskForm):
+    feedback_redirect = SubmitField('Anonymous Feedback')
+
+
+class FeedbackForm(FlaskForm):
+    section = SelectField('Lab Section',
+                          choices=[('P01', 'P01 Monday 1:05pm-3:55pm'), ('Q05', 'Q05 Wednesday 1:05pm-3:55pm')])
+    feedback = TextAreaField('Feedback', validators=[DataRequired()])
+    submit = SubmitField('Submit')
