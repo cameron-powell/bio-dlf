1cc3b5a7d6f0676626ad04c03675adfaeac99a26
Cameron <cameronspowell@gmail.com>
2017-03-02 22:47:04 -0500

    Updated to use the blueprint created in main

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,8 @@ def create_app(config_name):
     bootstrap.init_app(app)
     mail.init_app(app)
 
-    # attach routes and custom error pages here
+    # Attach routes and custom error pages here.
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
 
     return app
